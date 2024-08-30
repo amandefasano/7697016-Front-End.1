f408d0c5e6fbdf1091da92a2b497ab9a9a02dcd1
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-08-30 09:01:32 +0300

    Add: get the article's review with the api if doesn't already exist in the local storage

--- avis.js
+++ avis.js
@@ -1,46 +1,52 @@
 export function ajoutListenersAvis() {
+  const piecesElements = document.querySelectorAll(".fiches article button");
 
-    const piecesElements = document.querySelectorAll(".fiches article button");
- 
-    for (let i = 0; i < piecesElements.length; i++) {
- 
-     piecesElements[i].addEventListener("click", async function (event) {
- 
-        const id = event.target.dataset.id;
-        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
-        const avis = await reponse.json();
-        const pieceElement = event.target.parentElement;
+  for (let i = 0; i < piecesElements.length; i++) {
+    piecesElements[i].addEventListener("click", async function (event) {
+      const id = event.target.dataset.id;
 
-        const avisElement = document.createElement("p");
-        for (let i = 0; i < avis.length; i++) {
-            avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
-        }
-        pieceElement.appendChild(avisElement);
- 
-     });
- 
-    }
- }
- 
- export function ajoutListenerEnvoyerAvis() {
-    const formulaireAvis = document.querySelector(".formulaire-avis");
-    formulaireAvis.addEventListener("submit", function (event) {
+      let localAvis = window.localStorage.getItem(`avis-${id}`);
+
+      if (localAvis === null) {
+        const reponse = await fetch(
+          "http://localhost:8081/pieces/" + id + "/avis"
+        );
+        localAvis = await reponse.json();
+        const valeurLocalAvis = JSON.stringify(localAvis);
+        window.localStorage.setItem(`avis-${id}`, valeurLocalAvis);
+      } else {
+        localAvis = JSON.parse(localAvis);
+      }
+      
+      const pieceElement = event.target.parentElement;
+
+      const avisElement = document.createElement("p");
+      for (let i = 0; i < localAvis.length; i++) {
+        avisElement.innerHTML += `<b>${localAvis[i].utilisateur}:</b> ${localAvis[i].commentaire} <br>`;
+      }
+      pieceElement.appendChild(avisElement);
+    });
+  }
+}
+
+export function ajoutListenerEnvoyerAvis() {
+  const formulaireAvis = document.querySelector(".formulaire-avis");
+  formulaireAvis.addEventListener("submit", function (event) {
     event.preventDefault();
     // Création de l’objet du nouvel avis.
     const avis = {
-        pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-        utilisateur: event.target.querySelector("[name=utilisateur]").value,
-        commentaire: event.target.querySelector("[name=commentaire]").value,
-        nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
+      pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+      utilisateur: event.target.querySelector("[name=utilisateur]").value,
+      commentaire: event.target.querySelector("[name=commentaire]").value,
+      nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value),
     };
     // Création de la charge utile au format JSON
     const chargeUtile = JSON.stringify(avis);
     // Appel de la fonction fetch avec toutes les informations nécessaires
     fetch("http://localhost:8081/avis", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
     });
-    });
-    
- }
+  });
+}
